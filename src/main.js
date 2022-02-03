8ee324e25c4169518e9481e385fffa1203ec9c85
weibo <919006380@qq.com>
2022-02-04 01:29:40 +0800

    添加百度统计

--- src/main.js
+++ src/main.js
@@ -1,21 +1,23 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
-import router from './router'
-import mitt from 'mitt'
-import 'normalize.css'
-import '../public/iconfont/iconfont.js'
-import * as Icons from '@element-plus/icons-vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+import router from "./router";
+import mitt from "mitt";
+import "normalize.css";
+import "../public/iconfont/iconfont.js";
+import * as Icons from "@element-plus/icons-vue";
 import "@antv/x6-vue-shape";
 
 const emitter = mitt();
-const app = createApp(App)
-Object.keys(Icons).forEach(key => {
-    app.component(key, Icons[key])
-})
-app.config.globalProperties.$EventBus = emitter
-app.use(router)
-app.use(ElementPlus)
-app.mount('#app')
+const app = createApp(App);
+Object.keys(Icons).forEach((key) => {
+  app.component(key, Icons[key]);
+});
+app.config.globalProperties.$EventBus = emitter;
+app.use(router);
+app.use(ElementPlus);
+app.mount("#app");
 
+
+ 
