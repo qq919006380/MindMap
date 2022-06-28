d67ffa3106766c02998b19261514bf60d3ac1cc1
weibo <919006380@qq.com>
2022-06-28 23:31:44 +0800

    add

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,25 @@
 import { createApp } from "vue";
 import store from './store'
 import App from "./App.vue";
-import ElementPlus from "element-plus";
-import "element-plus/dist/index.css";
+// import ElementPlus from "element-plus";
+// import "element-plus/dist/index.css";
 import router from "./router";
 import mitt from "mitt";
 import "normalize.css";
-import "../public/iconfont/iconfont.js";
-import * as Icons from "@element-plus/icons-vue";
+import "./iconfont/iconfont.js";
+// import * as Icons from "@element-plus/icons-vue";
 import "@antv/x6-vue-shape";
 
 
 const emitter = mitt();
 const app = createApp(App);
-console.log(Icons)
-Object.keys(Icons).forEach((key) => {
-    app.component(key, Icons[key as keyof typeof Icons]);
-});
+
+// Object.keys(Icons).forEach((key) => {
+//     app.component(key, Icons[key as keyof typeof Icons]);
+// });
 app.config.globalProperties.$EventBus = emitter;
 app.use(router);
-app.use(ElementPlus);
+// app.use(ElementPlus);
 
 app.use(store)// 将 store 实例作为插件安装
 app.mount("#app");
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,34 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
+const AutoImport = require("unplugin-auto-import/webpack");
+const Components = require("unplugin-vue-components/webpack");
+const { ElementPlusResolver } = require("unplugin-vue-components/resolvers");
+const CompressionPlugin = require("compression-webpack-plugin");
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  productionSourceMap: false,
+  configureWebpack: {
+    plugins: [
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+      new CompressionPlugin({
+        test: /\.js$|\.html$|\.css/, //匹配文件名
+        threshold: 10240, //对超过10k的数据进行压缩
+        deleteOriginalAssets: false, //是否删除原文件
+      }),
+    ],
+  },
+  chainWebpack: (config) => {
+    /* 添加分析工具*/
+    if (process.env.NODE_ENV === "production") {
+      if (process.env.npm_config_report) {
+        config.plugin("webpack-bundle-analyzer").use(require("webpack-bundle-analyzer").BundleAnalyzerPlugin).end();
+        config.plugins.delete("prefetch");
+      }
+    }
+  },
+});
